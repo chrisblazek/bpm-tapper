ca57aada5f9ab475cf4b30d00b9322abd4295afb
Chris Blazek <chris@blazek.me>
2016-04-16 01:09:12 -0600

    Implement basic functionality

--- src/bg/background.js
+++ src/bg/background.js
@@ -1,3 +1,27 @@
+var RESET_TIME = 3000;
+var timeout;
+var lastPress = Date.now();
+var average = 0;
+var iterations = 1;
+
 chrome.browserAction.onClicked.addListener(function() {
-  chrome.browserAction.setBadgeText({ text: "128" });
+  var elapsedTime = Date.now() - lastPress;
+  lastPress = Date.now();
+
+  if (elapsedTime > RESET_TIME) {
+    average = 0;
+    iterations = 1;
+  } else {
+    average = (average * (iterations - 1) + elapsedTime) / iterations;
+    iterations += 1;
+    var bpm = Math.round(60000 / average);
+    chrome.browserAction.setBadgeText({ text: bpm.toString() });
+  }
+
+  if (timeout) {
+    clearTimeout(timeout);
+  }
+  timeout = setTimeout(function() {
+    chrome.browserAction.setBadgeText({ text: "" });
+  }, RESET_TIME);
 });
