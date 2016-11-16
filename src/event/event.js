229d91cad11274b1d00e9b00b3da01675c2eb5f8
Chris Blazek <chris@blazek.me>
2016-11-16 09:34:17 -0700

    Remove event page and update message

--- src/event/event.js
+++
@@ -1,5 +0,0 @@
-chrome.runtime.onInstalled.addListener((details) => {
-    if (details.reason === 'update' && details.previousVersion === '1.0.1') {
-        chrome.tabs.create({ url: 'src/welcome/welcome.html' });
-    }
-});
